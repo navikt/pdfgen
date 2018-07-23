adb0f47585279fcd72e7ac0ac0e62182fc1db6de
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-07-23 09:21:52 +0200

    Add a dynamic renderer with GET

--- src/main/kotlin/no/nav/pdfgen/Bootstrap.kt
+++ src/main/kotlin/no/nav/pdfgen/Bootstrap.kt
@@ -6,8 +6,10 @@ import com.github.jknack.handlebars.Context
 import com.github.jknack.handlebars.Handlebars
 import com.github.jknack.handlebars.Helper
 import com.github.jknack.handlebars.JsonNodeValueResolver
+import com.github.jknack.handlebars.Template
 import com.github.jknack.handlebars.io.FileTemplateLoader
 import com.github.jknack.handlebars.io.StringTemplateSource
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.log
 import io.ktor.http.ContentType
@@ -27,7 +29,10 @@ import io.prometheus.client.exporter.common.TextFormat
 import net.logstash.logback.argument.StructuredArguments.keyValue
 import org.jsoup.Jsoup
 import org.jsoup.helper.W3CDom
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.format.DateTimeFormatter
 import kotlin.streams.toList
@@ -35,32 +40,17 @@ import kotlin.streams.toList
 val collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
 val objectMapper: ObjectMapper = ObjectMapper()
 val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+val templateRoot: Path = Paths.get("templates/")
+val handlebars: Handlebars = Handlebars(FileTemplateLoader(templateRoot.toFile())).apply {
+    registerHelper("iso_to_nor_date", Helper<String> {
+        context, _ ->
+        if (context == null) "" else dateFormat.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(context))
+    })
+}
+val log: Logger = LoggerFactory.getLogger("pdf-gen")
 
 fun main(args: Array<String>) {
-    val templateRoot = Paths.get("templates/")
-    val handlebars = Handlebars(FileTemplateLoader(templateRoot.toFile())).apply {
-        registerHelper("iso_to_nor_date", Helper<String> {
-            context, _ ->
-            if (context == null) "" else dateFormat.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(context))
-        })
-    }
-
-    val templates = Files.list(templateRoot)
-            .map {
-                it.fileName.toString() to Files.list(it).filter { it.fileName.extension == "hbs" }
-            }
-            .flatMap {
-                (applicationName, templateFiles) ->
-                templateFiles.map {
-                    val fileName = it.fileName.toString()
-                    val templateName = fileName.substring(0..fileName.length - 5)
-                    val templateBytes = Files.readAllBytes(it).toString(Charsets.UTF_8)
-                    val xhtml = handlebars.compile(StringTemplateSource(fileName, templateBytes))
-                    (applicationName to templateName) to xhtml
-                }
-            }
-            .toList()
-            .toMap()
+    val templates = loadTemplates()
 
     embeddedServer(Netty, 8080) {
         routing {
@@ -76,28 +66,63 @@ fun main(args: Array<String>) {
                     TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
                 }
             }
+            get("/api/v1/genpdf/{applicationName}/{template}") {
+                val template = call.parameters["template"]
+                val applicationName = call.parameters["applicationName"]
+                val dataFile = Paths.get("data", applicationName, "$template.json")
+                val data = if (Files.exists(dataFile)) {
+                    Files.readAllBytes(dataFile)
+                } else {
+                    "{}".toByteArray(Charsets.UTF_8)
+                }
+                render(loadTemplates(), call, objectMapper.readValue(data, JsonNode::class.java))
+            }
             post("/api/v1/genpdf/{applicationName}/{template}") {
-                val startTime = System.currentTimeMillis()
                 val jsonNode = objectMapper.readValue(call.receiveStream(), JsonNode::class.java)
                 println(objectMapper.writeValueAsString(jsonNode))
-                val template = call.parameters["template"]
-                val html = templates[call.parameters["applicationName"] to template]?.apply(Context
-                        .newBuilder(jsonNode)
-                        .resolver(JsonNodeValueResolver.INSTANCE)
-                        .build())
-                if (template != null && html != null) {
-                    log.debug("Generated HTML {}", keyValue("html", html))
-                    call.respondBytes(ContentType.parse("application/pdf")) {
-                        val doc = Jsoup.parse(html)
-                        val w3doc = W3CDom().fromJsoup(doc)
-
-                        createPDFA(w3doc, template)
-                    }
-                    log.info("Done generating PDF in ${System.currentTimeMillis() - startTime}ms")
-                } else {
-                    call.respondText("Template or application not found", status = HttpStatusCode.NotFound)
-                }
+                render(templates, call, jsonNode)
             }
         }
     }.start(wait = true)
 }
+
+suspend fun render(templates: Map<Pair<String, String>, Template>, call: ApplicationCall, jsonNode: JsonNode) {
+    val startTime = System.currentTimeMillis()
+    val template = call.parameters["template"]
+    val html = templates[call.parameters["applicationName"] to template]?.apply(Context
+            .newBuilder(jsonNode)
+            .resolver(JsonNodeValueResolver.INSTANCE)
+            .build())
+    if (template != null && html != null) {
+        log.debug("Generated HTML {}", keyValue("html", html))
+        call.respondBytes(ContentType.parse("application/pdf")) {
+            val doc = Jsoup.parse(html)
+            val w3doc = W3CDom().fromJsoup(doc)
+
+            createPDFA(w3doc, template)
+        }
+        log.info("Done generating PDF in ${System.currentTimeMillis() - startTime}ms")
+    } else {
+        call.respondText("Template or application not found", status = HttpStatusCode.NotFound)
+    }
+}
+
+fun loadTemplates() = Files.list(templateRoot)
+        .filter {
+            !Files.isHidden(it)
+        }
+        .map {
+            it.fileName.toString() to Files.list(it).filter { it.fileName.extension == "hbs" }
+        }
+        .flatMap {
+            (applicationName, templateFiles) ->
+            templateFiles.map {
+                val fileName = it.fileName.toString()
+                val templateName = fileName.substring(0..fileName.length - 5)
+                val templateBytes = Files.readAllBytes(it).toString(Charsets.UTF_8)
+                val xhtml = handlebars.compile(StringTemplateSource(fileName, templateBytes))
+                (applicationName to templateName) to xhtml
+            }
+        }
+        .toList()
+        .toMap()
