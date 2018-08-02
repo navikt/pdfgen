2fc3e087b9f10b7533a5321403bc7854f072e208
Knut Nygaard <knutnyg@gmail.com>
2018-08-02 14:26:57 +0200

    Forbedret switch case

--- src/main/resources/Helpers.js
+++ src/main/resources/Helpers.js
@@ -1,13 +1,27 @@
-
-Handlebars.registerHelper("switch", function(value, options) {
+Handlebars.registerHelper("switch", function (value, options) {
     this._switch_value_ = value;
-    var html = options.fn(this); // Process the body of the switch block
+    this._switch_break_ = false;
+    var html = options.fn(this);
+    delete this._switch_break_;
     delete this._switch_value_;
     return html;
 });
 
-Handlebars.registerHelper("case", function(value, options) {
-    if (value == this._switch_value_) {
+Handlebars.registerHelper("case", function (value, options) {
+    var args = Array.prototype.slice.call(arguments);
+    var options = args.pop();
+    var caseValues = args;
+
+    if (this._switch_break_ || caseValues.indexOf(this._switch_value_) === -1) {
+        return '';
+    } else {
+        if (options.hash.break === true) {
+            this._switch_break_ = true;
+        }
         return options.fn(this);
     }
+});
+
+Handlebars.registerHelper('formatComma', function (number) {
+    return number.toString().replace('.', ',');
 });
