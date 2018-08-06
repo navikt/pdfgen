5e7dbf957047c301797d86002a874a99d68eb083
Kent Stefan Daleng <kent.stefan.daleng@nav.no>
2018-08-06 14:34:32 +0200

    Set logback to log at DEBUG-level

--- src/main/kotlin/no/nav/pdfgen/Bootstrap.kt
+++ src/main/kotlin/no/nav/pdfgen/Bootstrap.kt
@@ -115,7 +115,7 @@ fun initializeApplication(port: Int): ApplicationEngine {
                 val template = call.parameters["template"]!!
                 val applicationName = call.parameters["applicationName"]!!
                 val jsonNode = objectMapper.readValue(call.receiveStream(), JsonNode::class.java)
-                log.debug("JSON: {}", jsonNode.toString())
+                log.debug("JSON: {}", objectMapper.writeValueAsString(jsonNode))
                 render(applicationName, template, templates, jsonNode)?.let {
                     call.respond(PdfContent(it, template))
                     log.info("Done generating PDF in ${System.currentTimeMillis() - startTime}ms")
