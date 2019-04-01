81ade0517de4801af4fc46dbba25ab8bd96ebd45
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-04-01 09:09:10 +0200

    Bump to 1.1.7-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.pdfgen"
-version = "1.1.6"
+version = "1.1.7-SNAPSHOT"
 
 val handlebarsVersion = "4.1.2"
 val jacksonVersion = "2.9.8"
