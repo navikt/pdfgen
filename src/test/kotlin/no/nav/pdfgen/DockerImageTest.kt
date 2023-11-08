3190f0283e0ff8c5b68b978c89ec98901cdf43a1
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-11-08 19:22:40 +0100

    Update DockerImageTest.kt

--- src/test/kotlin/no/nav/pdfgen/DockerImageTest.kt
+++ src/test/kotlin/no/nav/pdfgen/DockerImageTest.kt
@@ -7,13 +7,13 @@ import org.testcontainers.containers.GenericContainer
 import java.nio.file.Paths
 
 internal class DockerImageTest {
-    private var pdfgen: GenericContainer
+    private var pdfgen: GenericContainer /
     GenericContainer(
-        ImageFromDockerfile = ImageFromDockerfile()
+     ImageFromDockerfile()
         .withFileFromPath("/", Paths.get("Dockerfile"))
         ).apply {
             start()
-        }
+         }
     @Test
     internal fun `Test Dockerfile`() {
         Assertions.assertEquals(true, pdfgen.isDeleteOnExit)
