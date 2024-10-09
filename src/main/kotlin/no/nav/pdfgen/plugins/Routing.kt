d135ee409fd37f083a571880de5dd9999508cbd6
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-09 10:34:18 +0200

    chore: small prometheus clean up

--- src/main/kotlin/no/nav/pdfgen/plugins/Routing.kt
+++ src/main/kotlin/no/nav/pdfgen/plugins/Routing.kt
@@ -5,10 +5,13 @@ import io.ktor.server.routing.*
 import no.nav.pdfgen.Environment
 import no.nav.pdfgen.application.api.pdf.registerGenerateHtmlApi
 import no.nav.pdfgen.application.api.pdf.registerGeneratePdfApi
+import no.nav.pdfgen.application.metrics.monitorHttpRequests
 
 fun Application.configureRouting(environment: Environment) {
     routing {
         route("/api/v1/genpdf") { registerGeneratePdfApi(environment) }
         route("/api/v1/genhtml") { registerGenerateHtmlApi(environment) }
     }
+    intercept(ApplicationCallPipeline.Monitoring, monitorHttpRequests())
+
 }
