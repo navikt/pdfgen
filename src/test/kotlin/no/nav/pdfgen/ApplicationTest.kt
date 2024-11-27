b1b6de4a821afbc789cb849c94639bede3e98272
inga <inga.kristine.holen@nav.no>
2024-11-27 15:35:37 +0100

    Tester for readyness- og livenesstester

--- src/test/kotlin/no/nav/pdfgen/ApplicationTest.kt
+++ src/test/kotlin/no/nav/pdfgen/ApplicationTest.kt
@@ -2,14 +2,56 @@ package no.nav.pdfgen
 
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.events.*
 import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
 import no.nav.pdfgen.application.api.nais.registerNaisApi
+import no.nav.pdfgen.plugins.configureLifecycleHooks
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class ApplicationTest {
+
+    @Test
+    internal fun `App is ready only after ServerReady is raised`() {
+        testApplication {
+            var raiseApplicationStarted: () -> Any
+            var raiseServerReady: () -> Any
+            application {
+                routing {
+                    val applicationState = ApplicationState()
+                    registerNaisApi(applicationState)
+                    configureLifecycleHooks(applicationState)
+                }
+                raiseApplicationStarted = { monitor.raise(ApplicationStarted, this) }
+                raiseServerReady = { monitor.raise(ServerReady, this.environment) }
+            }
+
+            assertNotReady()
+            raiseApplicationStarted()
+            assertNotReady()
+            raiseServerReady()
+            assertReady()
+        }
+    }
+
+    @Test
+    internal fun `App is alive after application is started`() {
+        testApplication {
+            application {
+                routing {
+                    val applicationState = ApplicationState()
+                    configureLifecycleHooks(applicationState)
+                    registerNaisApi(applicationState)
+                }
+            }
+            val response = client.get("/internal/is_alive")
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("I'm alive", response.bodyAsText())
+        }
+    }
 
     @Test
     internal fun `Returns ok on is_alive`() {
@@ -26,24 +68,6 @@ internal class ApplicationTest {
 
             assertEquals(HttpStatusCode.OK, response.status)
             assertEquals("I'm alive", response.bodyAsText())
-        }
-    }
-
-    @Test
-    internal fun `Returns ok in is_ready`() {
-        testApplication {
-            application {
-                routing {
-                    val applicationState = ApplicationState()
-                    applicationState.ready = true
-                    applicationState.alive = true
-                    registerNaisApi(applicationState)
-                }
-            }
-            val response = client.get("/internal/is_ready")
-
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertEquals("I'm ready", response.bodyAsText())
         }
     }
 
@@ -76,10 +100,20 @@ internal class ApplicationTest {
                     registerNaisApi(applicationState)
                 }
             }
-            val response = client.get("/internal/is_ready")
-
-            assertEquals(HttpStatusCode.InternalServerError, response.status)
-            assertEquals("Please wait! I'm not ready :(", response.bodyAsText())
+            assertNotReady()
         }
+
+    }
+
+    private suspend fun ApplicationTestBuilder.assertReady() {
+        val readyResponse = client.get("/internal/is_ready")
+        assertEquals(HttpStatusCode.OK, readyResponse.status)
+        assertEquals("I'm ready", readyResponse.bodyAsText())
+    }
+
+    private suspend fun ApplicationTestBuilder.assertNotReady() {
+        val response = client.get("/internal/is_ready")
+        assertEquals(HttpStatusCode.InternalServerError, response.status)
+        assertEquals("Please wait! I'm not ready :(", response.bodyAsText())
     }
 }
