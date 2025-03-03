5321af8a29d2efc0a5c1f80af2c3252be13cec2a
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2025-03-03 11:55:01 +0100

    chore: bumped pdfgen-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val junitJupiterVersion = "5.12.0"
 val verapdfVersion = "1.26.1"
 val ktfmtVersion = "0.44"
 val testcontainersVersion = "1.20.5"
-val pdfgencoreVersion = "1.1.42"
+val pdfgencoreVersion = "1.1.43"
 
 
 ///Due to vulnerabilities
