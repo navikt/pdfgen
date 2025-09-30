47c7667d9c643a91d8e1aefe370e4603ff3a9d8e
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2025-09-30 11:26:51 +0200

    chore: Bump pdfgen-core to 1.1.62 (#404)
    Updated pdfgencoreVersion to 1.1.62 to address vulnerabilities.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val junitJupiterVersion = "6.0.0"
 val verapdfVersion = "1.28.2"
 val ktfmtVersion = "0.44"
 val testcontainersVersion = "1.21.3"
-val pdfgencoreVersion = "1.1.61"
+val pdfgencoreVersion = "1.1.62"
 
 ///Due to vulnerabilities
 val commonsCompressVersion = "1.28.0"
